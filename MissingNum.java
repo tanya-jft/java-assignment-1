16bf378262a6ce248fc0ffe38787a3b7c9039dcd
tanya-jft <tanya.tomar@jellyfishtechnologies.com>
2023-03-22 10:41:07 +0530

    changes in missing num file

--- MissingNum.java
+++ MissingNum.java
@@ -1,11 +1,19 @@
+import java.util.Random;
+
 public class MissingNum {
     public static void main(String [] args) {
         int[] arr2 = new int[10];
         int randomNum = (int) (Math.random()*10) + 1;
+        System.out.println("Generated random num: " + randomNum);
+        // try Random class
+        Random random = new Random();
+        int missingNum  = 0;
 
         for(int i = 0; i < arr2.length; i++) {
-            arr2[i] = i + 1;
-            if (randomNum == i + 1) {
+            int x = random.nextInt(10) + 1;
+            arr2[i] = x;
+            if (randomNum == arr2[i]) {
+                missingNum = arr2[i];
                 arr2[i] = 0;
             }
         }
@@ -14,6 +22,6 @@ public class MissingNum {
             System.out.print(x + " ");
         }
         System.out.println();
-        System.out.println("Missing value: " + randomNum);
+        System.out.println("Missing value: " + missingNum);
     }
 }
