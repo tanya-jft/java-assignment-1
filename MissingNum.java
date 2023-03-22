a8c605f78374a3195dea238edf68b4aea2141773
tanya-jft <tanya.tomar@jellyfishtechnologies.com>
2023-03-22 15:49:21 +0530

    recheck the changes in missing number

--- MissingNum.java
+++ MissingNum.java
@@ -1,27 +1,49 @@
+import java.util.InputMismatchException;
 import java.util.Random;
-
+import java.util.Scanner;
 public class MissingNum {
+
     public static void main(String [] args) {
-        int[] arr2 = new int[10];
-        int randomNum = (int) (Math.random()*10) + 1;
-        System.out.println("Generated random num: " + randomNum);
-        // try Random class
-        Random random = new Random();
-        int missingNum  = 0;
-
-        for(int i = 0; i < arr2.length; i++) {
-            int x = random.nextInt(10) + 1;
-            arr2[i] = x;
-            if (randomNum == arr2[i]) {
-                missingNum = arr2[i];
-                arr2[i] = 0;
+        Scanner scanner = new Scanner(System.in);
+        int randomNum = (int)(Math.random()*10) + 1; // random num
+        int [] arr = new int[10]; // arr declaration
+        System.out.println("Enter the value within 1-10 range");
+        int i = 0;
+        int size = 0;
+        int sum = 0;
+        // validate the input
+        while(true) {
+            if(size != 10 && size < 10) {
+                System.out.print("arr[" + size + "]: ");
+                try{
+                    int userInput = scanner.nextInt();
+                    if(userInput >= 1 && userInput <= 10) {
+                        arr[size] = userInput;
+                        size += 1;
+                        sum += userInput;
+                        continue;
+                    }
+                    System.out.println("Value must be within range");
+                } catch(InputMismatchException e) {
+                    System.out.println("Input type must be an Integer"); // type error
+                    scanner.nextLine(); // clear buffer
+                }
             }
+            else break;
         }
-
-        for(int x : arr2) {
-            System.out.print(x + " ");
+        // set 0 in place of element which matches to the random number
+        boolean randomnumMatchesArr = false;
+        int newSum = 0;
+        for(int j = 0; j < arr.length; j++) {
+            if(randomNum == arr[i]) {
+               randomnumMatchesArr = true;
+                arr[i] = 0;
+            }
+            newSum += arr[i];
         }
-        System.out.println();
-        System.out.println("Missing value: " + missingNum);
+        if(randomnumMatchesArr) {
+            System.out.println("Missing Number is " + (sum-newSum) );
+        }
+        else System.out.println("Input elements of array does not match to the generated random number");
     }
 }
